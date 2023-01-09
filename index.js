dea5428954341825d79126fe2da384cf44cdfc22
Flynn (Mr. Robot) <flynnwebdev@gmail.com>
2023-01-09 13:40:26 +1000

    Completed CRUD

--- index.js
+++ index.js
@@ -23,15 +23,22 @@ const entrySchema = new mongoose.Schema({
 // Create a Mongoose model based on the schema
 const EntryModel = mongoose.model('Entry', entrySchema)
 
+// Categories
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true }
+})
+
+const CategoryModel = mongoose.model('Category', categorySchema)
+
 
 const app = express()
 const port = 4001
 
 app.use(express.json())
 
-app.get('/', (request, response) => response.send({ info: 'Journal API' }))
+app.get('/', (request, response) => response.send({ info: 'Journal API 2023' }))
 
-app.get('/categories', (req, res) => res.send(categories))
+app.get('/categories', async (req, res) => res.send(await CategoryModel.find()))
 
 app.get('/entries', async (req, res) => res.send(await EntryModel.find()))
 
@@ -47,8 +54,41 @@ app.get('/entries/:id', async (req, res) => {
   catch (err) {
     res.status(500).send({ error: err.message })
   }
-
 })
+
+// Update
+app.put('/entries/:id', async (req, res) => {
+  const { category, content } = req.body
+  const newEntry = { category, content }
+  
+  try {
+    const entry = await EntryModel.findByIdAndUpdate(req.params.id, newEntry, { returnDocument: 'after' })
+    if (entry) {
+      res.send(entry)
+    } else {
+      res.status(404).send({ error: 'Entry not found' })
+    }
+  }
+  catch (err) {
+    res.status(500).send({ error: err.message })
+  }
+})
+
+// Delete
+app.delete('/entries/:id', async (req, res) => {
+  try {
+    const entry = await EntryModel.findByIdAndDelete(req.params.id)
+    if (entry) {
+      res.sendStatus(204)
+    } else {
+      res.status(404).send({ error: 'Entry not found' })
+    }
+  }
+  catch (err) {
+    res.status(500).send({ error: err.message })
+  }
+})
+
 
 app.post('/entries', async (req, res) => {
   try {
